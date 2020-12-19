896203915cd600e83e75b40408d6c404d10f7b77
AlexAks <axinav@gmail.com>
2020-12-19 20:56:09 +0300

    check_type complit

--- readf.py
+++ readf.py
@@ -20,8 +20,19 @@ class HtmlParcer(object):
         self.soup=bs(self.content, 'lxml')
 
     def check_type(self):
-        sp=re.search(r'СП \d*', self.soup.title.text, re.M|re.I)
-        if sp:
+    strs=re.search(r'\bСП|ГОСТ|СанПиН', self.soup.title.text, re.M)
+        if strs[0]=="СП":
             self.lawtype='SP'
+        elif strs[0]=="ГОСТ":
+            self.lawtype='GOST'
+        elif strs[0]=="СанПиН":
+            self.lawtype='SANPIN'
+            
+            
 
 
+
+if __name__ == "__main__":
+    html=HtmlParcer("~/coding/lawbrouser/LAW/Urban/Sp18_13330_2011.htm")
+    html.check_type()
+    print (html.lawtype)
